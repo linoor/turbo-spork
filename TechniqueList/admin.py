b4c9e8998fed929bd386fc4a2ab208dbf8bc1d97
AlphaOlish <olga.ishimbaeva@gmail.com>
2017-12-03 22:31:34 +0100

    Created models

--- TechniqueList/admin.py
+++ TechniqueList/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from TechniqueList.models import Position, Technique, Group
+
+admin.site.register(Position)
+admin.site.register(Group)
+admin.site.register(Technique)
--- TechniqueList/models.py
+++ TechniqueList/models.py
@@ -1,3 +1,25 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=200)
+    position = models.ForeignKey(Position)
+
+
+class Technique(models.Model):
+    # url: url from Youtube
+    # repetitions_done: numbers of repetitions that already done by user for this technique
+    # repetitions_goal: numbers of repetitions to do by user for this technique
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    url = models.CharField(max_length=200)
+    repetitions_done = models.IntegerField(default=0)
+    repetitions_goal = models.IntegerField(default=10)
+    date = models.DateTimeField(default=datetime.now)
+    deleted = models.BooleanField(default=False)
+    group = models.ForeignKey(Group)
